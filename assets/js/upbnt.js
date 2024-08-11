70412861070bf8174b1faecd42ca58bbe75c843c
Filipe Maciel <filipemaciellopes01@gmail.com>
2024-08-11 17:03:24 -0400

    add coisas

---
+++ assets/js/upbnt.js
@@ -0,0 +1,16 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("btn-topo").style.display = "block";
+  } else {
+    document.getElementById("btn-topo").style.display = "none";
+  }
+}
+
+document.getElementById("btn-topo").onclick = function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
